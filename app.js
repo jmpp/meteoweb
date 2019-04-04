04787349979e56232424e0acac35d16434b6ad1a
J.M. Cléry <jmclery@gmail.com>
2019-04-04 10:15:14 +0200

    Réponse de l'API ok

--- app.js
+++ app.js
@@ -9,6 +9,25 @@ $(document).ready(function() {
 
         const city = $ville.val();
         
-        console.log(`Recherche dans ${city}`);
+        onSearch(city)
+            .then(data => {
+                console.log(data)
+
+                let img = 'http://openweathermap.org/img/w/'+ data.weather[0].icon +'.png'
+
+                $('#results').html(`Météo dans la ville de : ${data.name}
+                                        <img src="${img}" alt="">`)
+            });
     });
 });
+
+const API_KEY = 'e868747d83176ba6ec5d9ce6d81423f0';
+const API_ENDPOINT = 'http://api.openweathermap.org/data/2.5';
+
+function onSearch(cityName) {
+    cityName = encodeURIComponent(cityName);
+    let promise = fetch(`${API_ENDPOINT}/weather?q=${cityName}&APPID=${API_KEY}&units=metric&lang=fr`)
+                    .then(res => res.json());
+
+    return promise;
+}
