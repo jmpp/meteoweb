b4eba7b957e7e051211017608d2c885ff671b158
J.M. Cléry <jmclery@gmail.com>
2019-04-04 11:40:25 +0200

    Affichage de la météo avec une card bootstrap

--- app.js
+++ app.js
@@ -3,28 +3,40 @@ $(document).ready(function() {
 
     const $formSearch = $('#formSearch');
     const $ville = $formSearch.find('#ville');
-
+    const $results = $('#results');
+    
     $formSearch.on('submit', function(event) {
         event.preventDefault();
 
         const city = $ville.val();
         
-        onSearch(city)
-            .then(data => {
-                console.log(data)
-
-                let img = 'http://openweathermap.org/img/w/'+ data.weather[0].icon +'.png'
-
-                $('#results').html(`Météo dans la ville de : ${data.name}
-                                        <img src="${img}" alt="">`)
-            });
+        getWeatherByCity(city).then(weather => {
+            $results.html(
+                `<div class="card" style="width: 22rem;">
+                    <div class="card-body">
+                        ${weather.weather.map(w => (
+                            `<img src="http://openweathermap.org/img/w/${w.icon}.png" alt="${w.description}" class="float-right">`
+                        )).join('')}
+                        <h5 class="card-title">${weather.name}, ${weather.sys.country}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">
+                            ${weather.weather.map(w => (
+                                `<strong>${w.description}</strong>`
+                            )).join(', ')}
+                            <br>
+                        </h6>
+                        <p class="card-text">Temperature : ${weather.main.temp}°C</p>
+                        <a href="#" class="card-link">Voir les prévisions</a>
+                    </div>
+                </div>`
+            );
+        });
     });
 });
 
 const API_KEY = 'e868747d83176ba6ec5d9ce6d81423f0';
 const API_ENDPOINT = 'http://api.openweathermap.org/data/2.5';
 
-function onSearch(cityName) {
+function getWeatherByCity(cityName) {
     cityName = encodeURIComponent(cityName);
     let promise = fetch(`${API_ENDPOINT}/weather?q=${cityName}&APPID=${API_KEY}&units=metric&lang=fr`)
                     .then(res => res.json());
